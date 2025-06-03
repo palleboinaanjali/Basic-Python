828438cacc54c8db875cf92d3170c458657d0c9d
palleboinaanjali <palleboinaanjali753@gmail.com>
2025-06-03 10:14:13 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,1 +1,12 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is Even")
+    else:
+        print(f"{number} is Odd")
+
+try:
+    num = int(input("Enter a number: "))
+    check_even_odd(num)
+except ValueError:
+    print("Please enter a valid integer.")
 
